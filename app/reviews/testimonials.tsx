3bc8ecaccb2224418acd23a74e8899187c6070a0
Vaggelis <vaggelis@amped.io>
2024-07-06 22:00:28 +0300

    initial setup

---
+++ app/reviews/testimonials.tsx
@@ -0,0 +1,14 @@
+function Testimonials() {
+  return (
+    <div>
+      <h1>Testimonials</h1>
+      <p>What our customers say about us</p>
+      <ul>
+        <li>Great food, great service!</li>
+        <li>Fast delivery, delicious food!</li>
+        <li>Best food in town!</li>
+      </ul>
+    </div>
+  );
+}
+export default Testimonials;
