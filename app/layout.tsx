775e8442f21e04fadcae3a1dcf0433649deaf656
Vaggelis Rigopoulos <vaggelis@amped.io>
2025-08-15 21:13:25 +0100

    add new gtm tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4X9NWXF5PE"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4X9NWXF5PE');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         {/* Google Tag Manager (noscript) */}
